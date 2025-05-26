84c686587e25f6e51e279b889015bb9ba73f4caf
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2025-05-27 02:48:19 +0530

    product component

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import { Slider } from "../component/popular";
 import CategoryBar from "./Home/CategoryBar";
 import { all, bear, burger, meat, noodles, pizza, vegetable } from "../assets";
 import ValuePackItems from "./Home/ValuePackItems";
+import { ProductList } from "./Home/ProductCard";
 
 const DummyCard = ({ title }) => (
   <div className="min-w-[200px] h-[220px] md:h-[500px] bg-zinc-200 dark:bg-zinc-700 rounded-lg flex items-center justify-center text-lg font-semibold text-zinc-800 dark:text-white shadow">
@@ -191,7 +192,123 @@ const Home = () => {
       onClick: () => console.log("View Roll Combo"),
     },
   ];
-  
+
+  const productData = [
+    {
+      name: "Chicken Burger",
+      restaurant: "Cookie Heaven",
+      rating: 4.5,
+      deliveryTime: 50,
+      calories: 500,
+      originalPrice: 60,
+      discountedPrice: 40,
+      imageUrl:
+        "https://kidoneo.com/wp-content/uploads/2023/07/Pizza_1688814259.jpg",
+    },
+    {
+      name: "BBQ Burger",
+      restaurant: "Cookie Heaven",
+      rating: 4.5,
+      deliveryTime: 50,
+      calories: 500,
+      originalPrice: 60,
+      discountedPrice: 40,
+      imageUrl:
+        "https://tse1.mm.bing.net/th?id=OIP.cDvnUwJJ8VWCoZ1GxL9AGgHaE8&pid=Api&P=0&h=180",
+    },
+    {
+      name: "Veggie Delight Pizza",
+      restaurant: "Pizza Palace",
+      rating: 4.8,
+      deliveryTime: 40,
+      calories: 700,
+      originalPrice: 350,
+      discountedPrice: 299,
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg",
+    },
+    {
+      name: "Paneer Tikka",
+      restaurant: "Spice Hub",
+      rating: 4.7,
+      deliveryTime: 30,
+      calories: 400,
+      originalPrice: 150,
+      discountedPrice: 120,
+      imageUrl:
+        "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?auto=format&fit=crop&w=800&q=80",
+    },
+    {
+      name: "Mutton Biryani",
+      restaurant: "Royal Feast",
+      rating: 4.9,
+      deliveryTime: 60,
+      calories: 900,
+      originalPrice: 250,
+      discountedPrice: 220,
+      imageUrl:
+        "https://static.wixstatic.com/media/91e241_b44aab0c38364d05b4d66a563af1aa6b~mv2.jpg/v1/fill/w_864,h_576,al_c,lg_1,q_85/91e241_b44aab0c38364d05b4d66a563af1aa6b~mv2.jpg",
+    },
+    {
+      name: "Masala Dosa",
+      restaurant: "South Spice",
+      rating: 4.6,
+      deliveryTime: 35,
+      calories: 350,
+      originalPrice: 90,
+      discountedPrice: 75,
+      imageUrl:
+        "https://tse3.mm.bing.net/th?id=OIP.XSCo5S6kP3o-7-jVqH4vGgHaE8&pid=Api&P=0&h=180",
+    },
+    {
+      name: "Chocolate Milkshake",
+      restaurant: "Cool Drinks",
+      rating: 4.3,
+      deliveryTime: 20,
+      calories: 450,
+      originalPrice: 80,
+      discountedPrice: 65,
+      imageUrl:
+        "https://www.mashed.com/img/gallery/chocolate-drizzle-strawberry-milkshake-recipe/l-intro-1682005343.jpg",
+    },
+    {
+      name: "Pasta Alfredo",
+      restaurant: "Italiano",
+      rating: 4.4,
+      deliveryTime: 40,
+      calories: 600,
+      originalPrice: 200,
+      discountedPrice: 180,
+      imageUrl:
+        "https://res.cloudinary.com/norgesgruppen/images/c_scale,dpr_auto,f_auto,q_auto:eco,w_1600/tulcxcntmwnys5ndgqvk/pasta-alfredo",
+    },
+    {
+      name: "Caesar Salad",
+      restaurant: "Green Bowl",
+      rating: 4.2,
+      deliveryTime: 25,
+      calories: 300,
+      originalPrice: 130,
+      discountedPrice: 110,
+      imageUrl:
+        "https://img.taste.com.au/zjjLh7F2/taste/2022/10/mexican-taco-chopped-salad-181812-1.jpg",
+    },
+    {
+      name: "Chicken Shawarma",
+      restaurant: "Middle East Flavors",
+      rating: 4.7,
+      deliveryTime: 45,
+      calories: 650,
+      originalPrice: 180,
+      discountedPrice: 160,
+      imageUrl:
+        "https://lh3.googleusercontent.com/-KMreWFpEnsE/XzocD52YYuI/AAAAAAAAE2Y/2n1Q5h3mzHYp018OpqpEzF_fDQbPoH3fwCLcBGAsYHQ/d/1597643785431800-0.png",
+    },
+  ];
+
+  const handleAddToCart = () => {
+    alert("added to cart");
+  };
 
   return (
     <div>
@@ -210,6 +327,11 @@ const Home = () => {
 
       <div className="p-4">
         <ValuePackItems valuePacks={DummyValuePacks} />
+      </div>
+
+      <div className="px-5">
+        <h2 className="text-2xl font-semibold ">Your Trusted Picks</h2>
+        <ProductList products={productData} handleAddToCart={handleAddToCart} />
       </div>
     </div>
   );
---
+++ client/src/Pages/Home/ProductCard.jsx
@@ -0,0 +1,103 @@
+import { Clock } from "lucide-react";
+import React from "react";
+
+const ProductCard = ({ product, onAddToCart }) => {
+  return (
+    <div className="bg-gray-50 relative rounded-xl shadow-md overflow-hidden flex flex-col transition hover:shadow-lg">
+      {/* Image */}
+      <div className="h-40 w-full">
+        <img
+          className="w-full h-full object-cover"
+          src={product.imageUrl}
+          alt={product.name}
+        />
+
+        <div className="absolute top-2 right-2 bg-green-600 text-white text-xs font-medium py-1 px-2 rounded shadow">
+          ⭐ {product.rating.toFixed(1)}
+        </div>
+      </div>
+      {/* <div className="relative w-full aspect-square bg-white">
+        <img
+          src={product.imageUrl}
+          alt={product.name}
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute top-2 right-2 bg-green-600 text-white text-xs font-medium py-0.5 px-2 rounded shadow">
+          ⭐ {product.rating.toFixed(1)}
+        </div>
+      </div> */}
+
+      {/* Product Details */}
+      <div className="p-4 flex flex-col flex-grow">
+        <h3 className="text-base font-semibold text-gray-800">
+          {product.name}
+        </h3>
+        <p className="text-sm text-gray-500 mb-2">{product.restaurant}</p>
+
+        {/* <div className="flex items-center text-xs text-gray-500 mb-3 space-x-3 flex-wrap">
+          <span className="flex items-center">
+            <svg
+              className="w-4 h-4 mr-1 fill-current text-indigo-500"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+              <path d="M12 7v5l4 2.5V12z" />
+            </svg>
+            {product.deliveryTime} min
+          </span>
+          <span className="flex items-center">
+            <svg
+              className="w-4 h-4 mr-1 fill-current text-indigo-500"
+              viewBox="0 0 24 24"
+            >
+              <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1s-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm7 16H5v-9h14v9zm0-11H5V5h14v3z" />
+            </svg>
+            {product.calories} Kcal
+          </span>
+        </div> */}
+
+        <div className="mt-auto flex items-center justify-between">
+          <div className="flex items-center mb-2">
+            {product.originalPrice && (
+              <span className="text-gray-500 line-through text-xs mr-2">
+                ₹{product.originalPrice}
+              </span>
+            )}
+            <span className="font-bold text-green-600 text-sm mr-2">
+              ₹{product.discountedPrice}
+            </span>
+            {product.deliveryTime && (
+              <div className="flex items-center text-gray-500 text-xs">
+                <Clock className="w-3 h-3 mr-1" />
+                <span>{product.deliveryTime} min</span>
+              </div>
+            )}
+
+          </div>
+          <button
+            onClick={() => onAddToCart(product)}
+            className="bg-green-600 text-white rounded-full w-9 h-9 flex items-center justify-center hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          >
+            <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
+              <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const ProductList = ({ products, handleAddToCart }) => {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 p-4">
+      {products?.map((product) => (
+        <ProductCard
+          key={product?.name}
+          product={product}
+          onAddToCart={handleAddToCart}
+        />
+      ))}
+    </div>
+  );
+};
